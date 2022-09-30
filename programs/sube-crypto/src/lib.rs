ad05be9474fc41111109e1da59fb67ba346d0578
Mateo Lafalce <mateolafalce3@gmail.com>
2022-09-30 18:36:39 -0300

    30/9/2022

--- programs/sube-crypto/src/lib.rs
+++ programs/sube-crypto/src/lib.rs
@@ -12,40 +12,78 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod sube_crypto {
     use super::*;
 
-    pub fn initialize_admin_account(
-        ctx: Context<InitializeAdminAccount>
+    pub fn initialize_bus_line(
+        ctx: Context<InitializeAdminAccount>,
+        0to3km: u64,
+        3to6km: u64,
+        6to12km: u64,
+        12to27km: u64,
+        more27km: u64
     ) -> Result<()> {
         let (services_pda, bump): (Pubkey, u8) = Pubkey::find_program_address(&[ctx.accounts.user.key().as_ref()], &Pubkey::from_str("Ca8tecWTapYzeGfa8FvAMSo6JCheTRPvQhsjebZm56YE").unwrap());
         let sube: &mut Account<SoLotery> = &mut ctx.accounts.sube;
         sube.authority = ctx.accounts.user.key();
         sube.bump_original = bump;
+        sube.prices = [0to3km, 3to6km, 6to12km, 12to27km, more27km];
         Ok(())
     }
     pub fn take_a_trip(
-        ctx: Context<Trip>
+        ctx: Context<Trip>,
+        km: u8,
     ) -> Result<()> {
-        //TODO
+        require!(km < 6,  Err(ErrorCode::OverdueCredits.into()));
+
         let _lamports: u64 = 1000000000;
         let from = &mut ctx.accounts.from;
         let to = &mut ctx.accounts.to;
         let transfer = system_instruction::transfer(
-            &from_pubkey, &to_pubkey, _lamports,
+            &from_pubkey, &to_pubkey, ctx.accounts.sube.prices[km],
         );
-        anchor_lang::solana_program::program::invoke(
-            &transfer,
-            &[from.to_account_info(), to.to_account_info().clone()],
-        ).expect("Error");
-        msg!("Transfered Lamports");
+        if km == 0 {
+            anchor_lang::solana_program::program::invoke(
+                &transfer,
+                &[from.to_account_info(), to.to_account_info().clone()],
+            ).expect("Error");
+            msg!("Transfered Lamports");
+        }
+        if km == 1 {
+            anchor_lang::solana_program::program::invoke(
+                &transfer,
+                &[from.to_account_info(), to.to_account_info().clone()],
+            ).expect("Error");
+            msg!("Transfered Lamports");
+        }
+        if km == 5 {
+            anchor_lang::solana_program::program::invoke(
+                &transfer,
+                &[from.to_account_info(), to.to_account_info().clone()],
+            ).expect("Error");
+            msg!("Transfered Lamports");
+        }
+        if km == 6 {
+            anchor_lang::solana_program::program::invoke(
+                &transfer,
+                &[from.to_account_info(), to.to_account_info().clone()],
+            ).expect("Error");
+            msg!("Transfered Lamports");
+        }
+        if km == 7 {
+            anchor_lang::solana_program::program::invoke(
+                &transfer,
+                &[from.to_account_info(), to.to_account_info().clone()],
+            ).expect("Error");
+            msg!("Transfered Lamports");
+        }
         Ok(())
     }
 }
 
 #[derive(Accounts)]
 pub struct InitializeAdminAccount<'info> {
-    #[account(init, seeds = [user.key().as_ref()], bump, payer = user, space = 41)]
+    #[account(init, seeds = [user.key().as_ref()], bump, payer = user, space = 85)]
     pub sube: Account<'info, SubeAdminAccount>,
     #[account(mut)]
-    pub user: Signer<'info>,
+    pub signer: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
 #[derive(Accounts)]
@@ -63,5 +101,10 @@ pub struct Trip<'info> {
 #[account]
 pub struct SubeAdminAccount {
     pub authority: Pubkey, 
-    pub bump_original: u8
+    pub bump_original: u8,
+    pub prices: Vec<u64>
+}
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Enter a value corresponding to your route")]InvalidaKilometer
 }
--- tests/TakeATrip.ts
+++ tests/TakeATrip.ts
@@ -3,22 +3,14 @@ import { Program } from "@project-serum/anchor";
 import { SubeCrypto } from "../target/types/sube_crypto";
 
 describe("sube-crypto", () => {
+  // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
+
   const program = anchor.workspace.SubeCrypto as Program<SubeCrypto>;
 
   it("Is initialized!", async () => {
-    const tx = await program.methods.initializeBusLine(
-      new anchor.BN(1785714),
-      new anchor.BN(2000000),
-      new anchor.BN(2100000),
-      new anchor.BN(2200000),
-      new anchor.BN(2300000),
-    )
-    .accounts({
-      sube: createAccountHolder.publicKey,
-      signer: provider.wallet.publicKey,
-      systemProgram: anchor.web3.SystemProgram.programId
-    }).rpc();
+    // Add your test here.
+    const tx = await program.methods.takeATrip().rpc();
     console.log("Your transaction signature", tx);
   });
 });
